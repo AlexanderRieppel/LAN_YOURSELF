83b54f652961bad9c373c7b03a6a789551ff0c8e
Dominik Backhausen <Dominik.Backhausen@gmail.com>
2014-04-08 17:01:30 +0200

    Extending ConfigEditor

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -7,9 +7,11 @@ import gui.MyPanel;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
 
 import ConfigEditor.ConfigEditor;
 import ConfigEditor.NoSuchFile;
+import ConfigEditor.WrongArgument;
 
 public class Controller {
 
@@ -31,24 +33,30 @@ public class Controller {
 	}
 	
 	public Controller(){
-		try {
-			ConfigEditor e = new ConfigEditor("tinc.conf");
-			e.readAll();
-			System.out.println(e.get("Subnet"));
-			e.set("Name", "Alex");
-			e.writeAll();
-		} catch (NoSuchFile e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-//		String url = "http://checkip.amazonaws.com";
-//		String ip = getPublicIP(url);
-//		if(ip != null){
-//			System.out.println(ip);
-//			MyPanel mg = new MyPanel();
-//			mg.textField_2.setText(ip);
-//			new MyFrame(mg, "Test GUI", true);
+//		try {
+//			ConfigEditor e = new ConfigEditor("tinc.conf");
+//			e.readAll();
+////			System.out.println(e.get("Subnet"));
+////			ArrayList<String> tt = new ArrayList<String>();
+////			for(int  i = 0; i < 100; i++)
+////				tt.add("Dominik"+i);
+//			e.remove("Interface");
+////			e.removeConections(tt);
+////			e.addConections(tt);
+//			e.writeAll();
+//		} catch (NoSuchFile e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
 //		}
+		String url = "http://checkip.amazonaws.com";
+		String ip = getPublicIP(url);
+		if(ip != null){
+			System.out.println(ip);
+			MyPanel mg = new MyPanel();
+			mg.textField_2.setText(ip);
+			new MyFrame(mg, "Test GUI", true);
+		}
+		
 	}
 
 }
